4c52246fcf818e38188abf251ec850a8db5d58b6
Vlad <knyazev.vlad03@gmail.com>
2023-03-13 22:02:28 +0200

    connect MongoDB to server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is started on ${PORT}`);
-})
+
+app.get('/', (req, res) => {
+    res.json({ a: 5 });
+});
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://vvknyazev:0GGL4Xb2Vz1jo72X@afterlifedb.vqwvtcg.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true
+        });
+        app.listen(PORT, (e) => {
+            if (e) console.log(e);
+            console.log('Server has been started on port: ' + PORT);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
