4845b512aed0201cf84d2179cc0e3ffa7ebfb75b
Vlad <knyazev.vlad03@gmail.com>
2023-03-23 01:30:27 +0200

    add first version of girl cards

--- client/src/components/Header/Card/Card.jsx
+++ client/src/components/Header/Card/Card.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import s from './Card.module.css'
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <div>
-            
+        <div className={s.card}>
+            <img src={props.img} alt="girl" className={s.cardImg}/>
+            <p className={s.name}>{props.name}</p>
+            <p className={s.desc}>{props.desc}</p>
         </div>
     );
 };
