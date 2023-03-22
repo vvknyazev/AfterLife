b1516579f91beed1b1e713eeea97a29f5b22640a
Vlad <knyazev.vlad03@gmail.com>
2023-03-22 14:20:12 +0200

    fixed menu wrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav/Nav";
+import Header from "./components/Header/Header";
+import React from "react";
 
 function App() {
-  return (
-    <Nav/>
-  );
+    return (
+        <div>
+            <Nav/>
+            <Header/>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -5,20 +5,22 @@ import n from './Nav.module.css'
 const Nav = () => {
     return (
         <div>
-            <Menu customBurgerIcon={<img src="/BurgerButton.png" alt={'burger icon'}/>}/>
+            <Menu customBurgerIcon={<img src="/BurgerButton.svg" alt={'burger icon'}/>}/>
             <div className={n.nav}>
                 <div>
-                    <a href="#"><img src="/Afterlife-logo.png" alt="logo" className={n.logo}/></a>
+                    <a href="#"><img src="/Afterlife-logo.svg" alt="logo" className={n.logo}/></a>
                     <ul>
                         <li><a href="#">Выбрать пару</a></li>
                         <li><a href="#">Подписки</a></li>
                         <li><a href="#">О нас</a></li>
                         <li><a href="#">FAQs</a></li>
                     </ul>
-                    <a href="#"><img src="/lang.png" alt="lang" className={n.lang}/></a>
-                    <a href="#"><img src="/profile.png" alt="profile" className={n.profile}/></a>
+                    <a href="#"><img src="/lang.svg" alt="lang" className={n.lang}/></a>
+                    <a href="#"><img src="/profile.svg" alt="profile" className={n.profile}/></a>
                 </div>
+
             </div>
+
         </div>
     );
 };
