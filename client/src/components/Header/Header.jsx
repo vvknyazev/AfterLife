bf4e2b98590bf13767badb5956299bd0ac61cd8b
Vlad <knyazev.vlad03@gmail.com>
2023-03-22 21:31:55 +0200

    add gif and title

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import style from './Header.module.css';
+import s from './Header.module.css';
 import Caregories from "./Categories/Caregories";
 
 const Header = () => {
     return (
-            <div className={style.header}>
-                <Caregories/>
-            </div>
+        <div className={s.header}>
+            <Caregories/>
+            <div><p className={s.title}>Мы поможем найти вам идеальную игровую пару</p></div>
+            <div className={'tenor-gif-embed'  + ' ' + s.gif} data-postid="27127278" data-share-method="host" data-aspect-ratio="1.35021"
+                 data-width="30%"><a
+                href="https://tenor.com/view/you-look-lonely-i-can-fix-that-bladerunner2049-gif-27127278">You Look
+                Lonely I Can Fix That Bladerunner2049 GIF</a>from <a
+                href="https://tenor.com/search/you+look+lonely+i+can+fix+that-gifs">You Look Lonely I Can Fix That
+                GIFs</a></div>
+
+        </div>
     );
 };
 
