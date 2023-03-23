adf09277cfc6faaaaa14d0487aeb74d6c128d05e
Vlad <knyazev.vlad03@gmail.com>
2023-03-24 01:42:29 +0200

    close dropdown menu if you click outside of it

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {slide as Menu} from 'react-burger-menu'
 import {ReactComponent as CaretIcon} from '../../icons/profile.svg';
 import {ReactComponent as SignUpIcon} from '../../icons/signup.svg';
@@ -8,23 +8,31 @@ import {CSSTransition} from 'react-transition-group';
 
 function NavItem(props) {
     const [open, setOpen] = useState(false);
+    const [isFistTimeOpen, setIsFistTimeOpen] = useState(false);
 
-    return (
-        <div>
-            <a href="#" className={n.profile} onClick={() => setOpen(!open)}>
-                {props.icon}
-            </a>
-
-            {open && props.children}
-        </div>
-    );
-}
-
-function DropdownMenu() {
     const [activeMenu, setActiveMenu] = useState('main');
     const [menuHeight, setMenuHeight] = useState(null);
     const dropdownRef = useRef(null);
 
+    // useOnClickOutside(dropdownRef, () => setOpen(false));
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            console.log(dropdownRef.current.contains(event.target));
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                console.log(dropdownRef.current);
+
+                setOpen(false);
+                setIsFistTimeOpen(true);
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [dropdownRef]);
 
     function calcHeight(el) {
         const height = el.offsetHeight;
@@ -39,30 +47,53 @@ function DropdownMenu() {
             </a>
         );
     }
-
+    function toggleFirstTimeOpenVar(){
+        setIsFistTimeOpen(false);
+    }
     return (
-        <div className="dropdown" style={{height: menuHeight}} ref={dropdownRef}>
+        <div>
+            {!isFistTimeOpen &&
+                <a href="#" className={n.profile} onClick={() => setOpen(!open)}>
+                    {props.icon}
+                </a>
+            }
+            {isFistTimeOpen &&
+                <a href="#" className={n.profile} onClick={() => {
+                    toggleFirstTimeOpenVar();
+                    setOpen(true)
+                }
+                }>
+                    {props.icon}
+                </a>
+            }
 
-            <CSSTransition
-                in={activeMenu === 'main'}
-                timeout={500}
-                classNames="menu-primary"
-                unmountOnExit
-                onEnter={calcHeight}>
-                <div className="menu">
-                    <DropdownItem
-                        leftIcon={<SignUpIcon/>}
-                    >
-                        Sign Up
-                    </DropdownItem>
-                    <DropdownItem
-                        leftIcon={<LoginIcon/>}
-                    >
-                        Log In
-                    </DropdownItem>
-                </div>
-            </CSSTransition>
+
+
+            {open &&
+                <div className="dropdown" style={{height: menuHeight}} ref={dropdownRef}>
+
+                    <CSSTransition
+                        in={activeMenu === 'main'}
+                        timeout={500}
+                        classNames="menu-primary"
+                        unmountOnExit
+                        onEnter={calcHeight}>
+                        <div className="menu">
+                            <DropdownItem
+                                leftIcon={<SignUpIcon/>}
+                            >
+                                Sign Up
+                            </DropdownItem>
+                            <DropdownItem
+                                leftIcon={<LoginIcon/>}
+                            >
+                                Log In
+                            </DropdownItem>
+                        </div>
+                    </CSSTransition>
+                </div>}
         </div>
+
     );
 }
 
@@ -81,10 +112,7 @@ const Nav = () => {
                     </ul>
                     <a href="#"><img src="/lang.svg" alt="lang" className={n.lang}/></a>
                     {/*<a href="#"><img src="/profile.svg" alt="profile" className={n.profile}/></a>*/}
-                    <NavItem icon={<CaretIcon/>}>
-                        <DropdownMenu></DropdownMenu>
-                    </NavItem>
-
+                    <NavItem icon={<CaretIcon/>}/>
                 </div>
 
             </div>
