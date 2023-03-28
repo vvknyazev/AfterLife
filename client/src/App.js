152c76fc0c0a3982c89e91d6b16c8ab96ab34a1b
Vlad <knyazev.vlad03@gmail.com>
2023-03-29 02:05:12 +0300

    add Strip component to site

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
 import React from "react";
+import Strip from "./components/Strip/Strip";
 
 function App() {
     return (
         <div>
             <Nav/>
             <Header/>
+            <Strip/>
+
         </div>
     );
 }
---
+++ client/src/components/Strip/Strip.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import s from './Strip.module.css'
+
+const Strip = (props) => {
+    return (
+        <div>
+            <div className={s.marqueeInfinite}>
+                <div className={s.anim}>
+                    <span>
+                        <img src='/strip/strip1.svg' alt=""/>
+                    </span>
+                    <span>
+                        <img src='/strip/strip1.svg' alt=""/>
+                    </span>
+                </div>
+            </div>
+            <div className={s.marqueeInfinite}>
+                <div className={s.anim2}>
+                    <span>
+                        <img src='/strip/strip22.svg' alt=""/>
+                    </span>
+                    <span>
+                        <img src='/strip/strip22.svg' alt=""/>
+                    </span>
+                </div>
+            </div>
+            <div className={s.marqueeInfinite}>
+                <div className={s.anim3}>
+                    <span>
+                        <img src='/strip/strip3.svg' alt=""/>
+                    </span>
+                    <span>
+                        <img src='/strip/strip3.svg' alt=""/>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Strip;
