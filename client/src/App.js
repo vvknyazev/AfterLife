667e33db408c954b01e75a3b2b29d7deab6bc5f6
Vlad <knyazev.vlad03@gmail.com>
2023-03-29 23:38:43 +0300

    add new component

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
 import React from "react";
 import Strip from "./components/Strip/Strip";
+import GirlsSection from "./components/GirlsSection/GirlsSection";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <div>
             <Nav/>
             <Header/>
+            <GirlsSection/>
             <Strip/>
-
+            <Footer/>
         </div>
     );
 }
