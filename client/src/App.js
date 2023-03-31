67fd256fff7022476d2152b222cba280f05f952d
Vlad <knyazev.vlad03@gmail.com>
2023-03-31 17:39:37 +0300

    add loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Strip from "./components/Strip/Strip";
 import GirlsSection from "./components/GirlsSection/GirlsSection";
 import Footer from "./components/Footer/Footer";
 import Concept from "./components/Concept/Concept";
+import {InfinitySpin} from 'react-loader-spinner'
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setIsLoading(false), 1000); // имитируем загрузку страницы
+    }, []);
     return (
         <div>
-            <Nav/>
-            <Header/>
-            <Concept/>
-            <GirlsSection/>
-            <Strip/>
-            <Footer/>
+            {isLoading ? (
+                <div className={'loader'}>
+                    <InfinitySpin
+                        width='200'
+                        color="#000"
+                    />
+                </div>
+            ) : (
+                <div>
+                    <Nav/>
+                    <Header/>
+                    <Concept/>
+                    <GirlsSection/>
+                    <Strip/>
+                    <Footer/>
+                </div>
+            )}
         </div>
     );
 }
