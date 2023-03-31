a8bf46f3166bd819f1462e1302ded64e558fa511
Vlad <knyazev.vlad03@gmail.com>
2023-03-31 16:50:25 +0300

    add concept component

--- client/src/components/Concept/Concept.jsx
+++ client/src/components/Concept/Concept.jsx
@@ -6,7 +6,16 @@ const Concept = () => {
         <div className={s.container}>
             <h1>Концепция</h1>
             <div className={s.concept}>
-
+                <div className={s.defaultService}>
+                    <div>
+                        <img src="/two_of_us.png" alt="pic"/>
+                    </div>
+                    <div><img src="/double_joint.png" alt="pic"/></div>
+                    <div><img src="/not_alone.png" alt="pic"/></div>
+                </div>
+                <div className={s.streamService}>
+                    <img src="/Welcome_to_AfterLife.png" alt="stream-service"/>
+                </div>
             </div>
         </div>
     );
