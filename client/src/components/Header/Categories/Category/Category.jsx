6a8cc655cfeacbedb09dc5b3d2fe22d08f84801d
Vlad <knyazev.vlad03@gmail.com>
2023-03-31 14:27:01 +0300

    delete console.log

--- client/src/components/Header/Categories/Category/Category.jsx
+++ client/src/components/Header/Categories/Category/Category.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import s from './Category.module.css'
 
 const Category = (props) => {
-    console.log(props.props);
     return (
         <div className={s.box}>
             <img src={props.games.img} alt="logo"/>
