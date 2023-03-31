6fc68a1d029624e36d01f222916a84ca723b7fe2
Vlad <knyazev.vlad03@gmail.com>
2023-03-31 14:27:25 +0300

    change strip svg to png

--- client/src/components/Strip/Strip.jsx
+++ client/src/components/Strip/Strip.jsx
@@ -7,30 +7,30 @@ const Strip = () => {
             <div className={s.marqueeInfinite}>
                 <div className={s.anim}>
                     <span>
-                        <img src='/strip/strip1.svg' alt=""/>
+                        <img src='/strip/strip1.png' alt=""/>
                     </span>
                     <span>
-                        <img src='/strip/strip1.svg' alt=""/>
+                        <img src='/strip/strip1.png' alt=""/>
                     </span>
                 </div>
             </div>
             <div className={s.marqueeInfinite}>
                 <div className={s.anim2}>
                     <span>
-                        <img src='/strip/strip22.svg' alt=""/>
+                        <img src='/strip/strip2.png' alt=""/>
                     </span>
                     <span>
-                        <img src='/strip/strip22.svg' alt=""/>
+                        <img src='/strip/strip2.png' alt=""/>
                     </span>
                 </div>
             </div>
             <div className={s.marqueeInfinite}>
                 <div className={s.anim3}>
                     <span>
-                        <img src='/strip/strip3.svg' alt=""/>
+                        <img src='/strip/strip3.png' alt=""/>
                     </span>
                     <span>
-                        <img src='/strip/strip3.svg' alt=""/>
+                        <img src='/strip/strip3.png' alt=""/>
                     </span>
                 </div>
             </div>
