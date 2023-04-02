54fa0bcbee3c7882a4e9f96d186ad75ace497814
Vlad <knyazev.vlad03@gmail.com>
2023-04-03 00:28:06 +0300

    add some middlewares into index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require('./routes/index');
+const path = require("path");
+const cors = require('cors')
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.json({ a: 5 });
-});
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+
+app.use(express.urlencoded({extended: true}))
+app.use('/api', router)
 async function start() {
     try {
         await mongoose.connect('mongodb+srv://vvknyazev:0GGL4Xb2Vz1jo72X@afterlifedb.vqwvtcg.mongodb.net/?retryWrites=true&w=majority', {
