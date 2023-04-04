2aac89f10ea37f4f84a31b857133533f3669d225
Vlad <knyazev.vlad03@gmail.com>
2023-04-04 16:29:43 +0300

    setup cors configuration

--- server/index.js
+++ server/index.js
@@ -3,21 +3,28 @@ const mongoose = require('mongoose');
 const router = require('./routes/index');
 const path = require("path");
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const credentials = require("./middlware/credentials");
+const corsOptions = require("./config/corsOptions");
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors())
+app.use(credentials);
+app.use(cors(corsOptions));
+
+
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
 app.use('/api', router)
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://vvknyazev:0GGL4Xb2Vz1jo72X@afterlifedb.vqwvtcg.mongodb.net/?retryWrites=true&w=majority', {
+        await mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true
         });
         app.listen(PORT, (e) => {
