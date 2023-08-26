7a6e74f5e776e47920a69d8b3b6856ff0d9562ce
vvknyazev <knyazev.vlad03@gmail.com>
2023-08-26 12:34:22 +0300

    update allowedOrigins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://localhost:5000 ',
+    'http://localhost:5001 ',
     'https://google.com',
     'https://accounts.google.com',
 ];
