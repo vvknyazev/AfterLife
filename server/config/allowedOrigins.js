5f6eb52231ede110ea01c41b4627537df583f5ac
vvknyazev <knyazev.vlad03@gmail.com>
2024-05-06 19:29:58 +0300

    update domain

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,4 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
@@ -8,6 +7,8 @@ const allowedOrigins = [
     'http://localhost',
     'https://localhost',
     'http://afterlife.pp.ua',
+    'http://afterlf.com',
+    'https://afterlf.com',
     'https://afterlife.pp.ua',
     'https://google.com',
     'https://accounts.google.com',
